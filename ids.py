cd4703c3553af44546dddadefc5c534d4fc00882
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2024-01-11 15:58:12 +0100

    OnAvance

--- ids.py
+++ ids.py
@@ -1,16 +1,17 @@
-import argparse
+from sys import argv
 import os
+import argparse
 
 # Argument 
 
 parser= argparse.ArgumentParser()
-parser.add_argument( "-b", "--build", default=0, const=1)
-parser.add_argument( "-c", "--checkout", default=0, const=1)
+parser.add_argument( "-b", "--build", default=False, help="construit un fichier JSON qui contient un état des choses qu'on a demandé à surveiller")
+parser.add_argument( "-c", "--check", default=False, help="vérifie que l'état actuel est conforme à ce qui a été stocké dans | /var/ids/db.json | ")
 arg = parser.parse_args()
 
-if arg.build == 1:
+if argv[1] == "build":
     print("build")
 
-if arg.check == 1:
+if argv[1] == "check":
     print("check")
 
