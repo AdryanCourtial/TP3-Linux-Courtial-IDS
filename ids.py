6643153014ddc7650da26c157b678ea764e55360
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2024-01-11 15:50:36 +0100

    OnAvance

--- ids.py
+++ ids.py
@@ -4,8 +4,8 @@ import os
 # Argument 
 
 parser= argparse.ArgumentParser()
-parser.add_argument("build", default=0, const=1)
-parser.add_argument("checkout", default=0, const=1)
+parser.add_argument( "-b", "--build", default=0, const=1)
+parser.add_argument( "-c", "--checkout", default=0, const=1)
 arg = parser.parse_args()
 
 if arg.build == 1:
