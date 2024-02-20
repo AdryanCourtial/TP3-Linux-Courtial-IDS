55843d37cc7ec4abdd030dcd111adfb0d7e8c533
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2024-02-20 14:08:38 +0100

    Report API

--- ids-api.py
+++ ids-api.py
@@ -1,21 +1,26 @@
 from flask import Flask
+from json import dumps
 from ids import check
-
 app = Flask(__name__)
 
-app.route('/check', methods=['POST'])
+@app.route('/check', methods=['POST'])
 def make_check():
-    return check()
+    return "<Reussi>"
 
-app.route('/reports', methods=['GET'])
+@app.route('/reports', methods=['GET'])
 def recup_reports():
-    ...
+    essaie = {
+        "data" : 10,
+        "pasData" : 20
+    }
+
+    return dumps(essaie)
 
 
-app.route('/reports/<id>', methods=['GET'])
+@app.route('/reports/<id>', methods=['GET'])
 def recup_report_by_id():
-    ...
+    return "report_by_id"
 
 
 if __name__ == '__main__':
-    app.run(host='10.1.1.100', port=80, debug=True)
+    app.run(host="0.0.0.0" , port=80)
