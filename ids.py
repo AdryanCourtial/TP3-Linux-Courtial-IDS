232032e5c715fb9a4d2a0f571e828b8d2324a890
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2024-02-21 15:36:04 +0100

    Report API

--- ids.py
+++ ids.py
@@ -377,7 +377,7 @@ def create_report_id(report):
     for i in fichier:
         fichier[fichier.index(i)] = delete_extenssion(i)
     fichier = sorted(fichier, reverse=True)
-    print("Use :" + fichier[1])
+    print("Last report :" + fichier[1])
 
 
 def delete_extenssion(f):
